d3f3cea86995388fa6a119df5faf8ea779315d2a
Dhruv Rohatgi <111676377+Courage003@users.noreply.github.com>
2023-05-20 12:11:21 +0530

    Minimum Triangulation of Polygon
    DP using Mamoization, Tabulation and Space reduced complexity

---
+++ dp14.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Minimum Triangulation of Polygon
+
+class Solution {
+public:
+    int solve(vector<int>&v, int i ,int j, vector<vector<int>>&dp){
+        //Mamoization
+        if(i+1==j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=INT_MAX;
+        for(int k=i+1;k<j;k++){
+            ans=min(ans,v[i]*v[j]*v[k]+solve(v,i,k,dp)+solve(v,k,j,dp));
+        }
+        return dp[i][j]=ans;
+    }
+
+    int solve1(vector<int>&v){
+        //Tabulation
+        int n=v.size();
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+2;j<n;j++){
+                int ans=INT_MAX;
+                for(int k=i+1;k<j;k++){
+                    ans=min(ans,v[i]*v[j]*v[k]+dp[i][k]+dp[k][j]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][n-1];
+    }
+    int minScoreTriangulation(vector<int>& values) {
+        int n=values.size();
+        //vector<vector<int>>dp(n,vector<int>(n,-1));
+        return solve1(values);
+        
+    }
+};
